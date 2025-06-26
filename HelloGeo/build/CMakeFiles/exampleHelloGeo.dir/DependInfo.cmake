56e269bc2c930f602134af57a67dd6abe13a37ae
geant4 <geant4@Andreas-MacBook-Pro-308.local>
2025-06-26 16:47:28 +0200

    Add HelloGeo example

---
+++ HelloGeo/build/CMakeFiles/exampleHelloGeo.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/exampleHelloGeo.cc" "CMakeFiles/exampleHelloGeo.dir/exampleHelloGeo.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/exampleHelloGeo.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/ActionInitialization.cc" "CMakeFiles/exampleHelloGeo.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/ActionInitialization.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/DetectorConstruction.cc" "CMakeFiles/exampleHelloGeo.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/DetectorConstruction.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/EventAction.cc" "CMakeFiles/exampleHelloGeo.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/EventAction.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleHelloGeo.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/RunAction.cc" "CMakeFiles/exampleHelloGeo.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/RunAction.cc.o.d"
+  "/Users/geant4/examples/GeoModelExamples/HelloGeo/src/SteppingAction.cc" "CMakeFiles/exampleHelloGeo.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/exampleHelloGeo.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HelloGeo/build/CMakeFiles/exampleHelloGeo.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleHelloGeo.dir/exampleHelloGeo.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/exampleHelloGeo.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/EventAction.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/RunAction.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/exampleHelloGeo.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/exampleHelloGeo.dir/src/SteppingAction.cc.o.d"
+  "exampleHelloGeo"
+  "exampleHelloGeo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleHelloGeo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
